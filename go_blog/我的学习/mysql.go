6ee82c3905fc7eb63b9053ee89ccf10242e2ceaf
NO5feng <97101453+NO5feng@users.noreply.github.com>
2022-09-20 16:47:35 +0800

    Update and rename go_blog/mysql/mysql.go to go_blog/我的学习/mysql.go

--- go_blog/我的学习/mysql.go
+++ go_blog/我的学习/mysql.go
@@ -11,7 +11,7 @@ var DB *sql.DB
 
 func init() {
 	// 执行main之前，先执行init方法
-	db, err := sql.Open("mysql", "root:205001@tcp(127.0.0.1:3306)/go_blog?charset=utf8&parseTime=True")
+	db, err := sql.Open("mysql", "root:xxxxx@tcp(127.0.0.1:3306)/go_blog?charset=utf8&parseTime=True")
 	if err != nil {
 		log.Println("数据库连接异常！！！")
 		panic(err)
